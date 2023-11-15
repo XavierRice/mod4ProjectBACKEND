9361c853af2adda9d238f78a5d156fc41e162bca
xavierrice <xavier.rice@gmail.com>
2023-11-15 13:50:07 -0500

    added seed

--- db/seed.sql
+++ db/seed.sql
@@ -16,4 +16,5 @@ INSERT INTO notes (user_id, subject_name, title, videos, content, subject_id)
 VALUES
 (1, 'React', 'React Basics: components, state and props', ARRAY['https://www.youtube.com/watch?v=omphzcP6wf4'], 'github link', 1),
 (2, 'Higher Order Functions', 'Functions: Cantrips and Useful Spells', ARRAY['https://www.youtube.com/results?search_query=react+prerequisites+js+higher+order+functions+javascript'], 'https://docs.google.com/document/d/1dsv2S1YGDP9plqR7UiOUkNuvp3y7lFsdtf4LWWIjLgs/edit', 2),
-(3, 'Figma', 'Figma Introduction: Beginner Collection', ARRAY['https://www.youtube.com/watch?v=Cx2dkpBxst8&list=PLXDU_eVOJTx7QHLShNqIXL1Cgbxj7HlN4'], 'https://docs.google.com/document/d/1X3kA9GPCDfHL-eQwTml7Ju9RyMreVAKy-KeD6qdpitw/edit', 1 );
+(1, 'Figma', 'Figma Introduction: Beginner Collection', ARRAY['https://www.youtube.com/watch?v=Cx2dkpBxst8&list=PLXDU_eVOJTx7QHLShNqIXL1Cgbxj7HlN4'], 'https://docs.google.com/document/d/1X3kA9GPCDfHL-eQwTml7Ju9RyMreVAKy-KeD6qdpitw/edit', 3 ),
+(1, 'React', 'React Hooks: There is more that use state?', ARRAY['https://www.youtube.com/watch?v=TNhaISOUy6Q'], 'https://docs.google.com/document/d/14MzdlZjcUq_1owCSotPfPgl2TVMALF1Anp6qMpV_wLk/edit', 1);
