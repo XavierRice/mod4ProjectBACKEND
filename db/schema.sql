ff51020f60fbb2783450948828780c2b214bee89
xavierrice <xavier.rice@gmail.com>
2023-11-15 12:15:24 -0500

    schema working

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS studybuddy_dev;
+CREATE DATABASE studybuddy_dev;
+
+\c studybuddy_dev;
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(25) NOT NULL,
+name TEXT DEFAULT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+member BOOLEAN NOT NULL,
+profilePic TEXT DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS subjects;
+
+CREATE TABLE subjects (
+    id SERIAL PRIMARY KEY,
+    subject_name TEXT UNIQUE
+);
+
+DROP TABLE IF EXISTS notes;
+
+CREATE TABLE notes(
+note_id SERIAL PRIMARY KEY,
+user_id INTEGER REFERENCES users (id),
+subject_name TEXT,
+title TEXT NOT NULL,
+videos TEXT[],
+content TEXT
+);
