6891077ddae09f2710728db360aacb6489e9607d
xavierrice <xavier.rice@gmail.com>
2023-11-20 13:16:10 -0500

    trying to connect

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,103 +1,97 @@
 //DEPENDENCIES
-const express = require('express');
-const notes = express.Router({mergeParams:true});
+const express = require("express");
+const notes = express.Router({ mergeParams: true });
 
 //QUERIES
 const {
-    getAllNotes,
-    getNote,
-    UpdateNote,
-    addVideotoNote,
-    CreateNote,
-    DeleteNote
-} = require('../queries/notes')
+  getAllNotes,
+  getNote,
+  UpdateNote,
+  addVideotoNote,
+  CreateNote,
+  DeleteNote,
+} = require("../queries/notes");
 
-const { getUser } = require('../queries/users');
+const { getUser } = require("../queries/users");
 
 //ROUTES
 
 //INDEX
 notes.get("/", async (req, res) => {
-    const { user_id } = req.params;
+  const { user_id } = req.params;
 
- try{
-     const allNotes = await getAllNotes(user_id);
-     const user = await getUser(user_id)
-     if(user.id){
-         res.status(200).json({...user, allNotes})
-        }else {
-        res.status(500).json({error: "user or notes not found"})
+  try {
+    const allNotes = await getAllNotes(user_id);
+    const user = await getUser(user_id);
+    if (user.id) {
+      res.status(200).json({ ...user, allNotes });
+    } else {
+      res.status(500).json({ error: "user or notes not found" });
     }
-}catch(error){
-    console.error("Error Fetching notes:", error)
-    res.status(500).json({error:"internal server error"})
-}});
+  } catch (error) {
+    console.error("Error Fetching notes:", error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
 
 //SHOW
 notes.get("/:note_id", async (req, res) => {
-    const { user_id, note_id } = req.params;
-    const user = await getUser(user_id);
-    const note = await getNote(note_id, user_id);
-    if(!note.note_id){
-        res.status(404).json({error:"Note not found", user_id, note_id})
-    }else{
-        if(user.id){
-            res.status(200).json({...user, note})
-           
-        } else {
-            res.status(404).json({error: "not found here"})
-        }
+  const { user_id, note_id } = req.params;
+  const user = await getUser(user_id);
+  const note = await getNote(note_id, user_id);
+  if (!note.note_id) {
+    res.status(404).json({ error: "Note not found", user_id, note_id });
+  } else {
+    if (user.id) {
+      res.status(200).json({ ...user, note });
+    } else {
+      res.status(404).json({ error: "not found here" });
     }
-    // if(user.id){
-    //     res.status(200).json({...user, note})
-       
-    // } else {
-    //     res.status(404).json({error: "not found here"})
-    // }
-})
+  }
+});
 
 //ADD VIDEOS
 notes.patch("/:note_id", async (req, res) => {
-    const { note_id } = req.params;
-    const { videoLink } = req.body;
-    try {
-        const addedVideo = await addVideotoNote(note_id, videoLink);
-        res.status(200).json({message:"Video is added"})
-    } catch(error){
-        console.error("Something went wrong!", error)
-        res.status(500).json({error: "Something broke:"})
-    }
-})
+  const { note_id } = req.params;
+  const { videoLink } = req.body;
+  try {
+    const addedVideo = await addVideotoNote(note_id, videoLink);
+    res.status(200).json({ message: "Video is added" });
+  } catch (error) {
+    console.error("Something went wrong!", error);
+    res.status(500).json({ error: "Something broke:" });
+  }
+});
 //UPDATE
-notes.put("/:note_id", async (req, res) =>{
-    const { id, note_id } = req.params;
-  
-    const updatedNote = await UpdateNote({note_id, id, ...req.body})
-    if (updatedNote.note_id){
-        res.status(200).json(updatedNote)
-    } else {
-        res.status(404).json("Error Sending")
-    }
+notes.put("/:note_id", async (req, res) => {
+  const { id, note_id } = req.params;
+
+  const updatedNote = await UpdateNote({ note_id, id, ...req.body });
+  if (updatedNote.note_id) {
+    res.status(200).json(updatedNote);
+  } else {
+    res.status(404).json("Error Sending");
+  }
 });
 
 //CREATE
 notes.post("/", async (req, res) => {
-    const { id } = req.params;
-    console.log(id)
-    const newNote = await CreateNote({id, ...req.body})
-    console.log(newNote)
-    res.status(200).json(newNote)
-})
+  const { id } = req.params;
+
+  const newNote = await CreateNote({ id, ...req.body });
+  console.log(newNote);
+  res.status(200).json(newNote);
+});
 
 // DELETE
 notes.delete("/:note_id", async (req, res) => {
-    const { note_id } = req.params;
-    const deletedNote = await DeleteNote(note_id);
-    if(deletedNote.note_id) {
-      res.status(200).json(deletedNote);
-    } else {
-      res.status(404).json({ error: "No note found" });
-    }
-  });
-  
-  module.exports = notes;
+  const { note_id } = req.params;
+  const deletedNote = await DeleteNote(note_id);
+  if (deletedNote.note_id) {
+    res.status(200).json(deletedNote);
+  } else {
+    res.status(404).json({ error: "No note found" });
+  }
+});
+
+module.exports = notes;
